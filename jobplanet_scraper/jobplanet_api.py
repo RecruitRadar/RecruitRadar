76c9e78be02adaa2bd0c12a132424b59a2393711
heewoneha <jhjmo0719h@naver.com>
2023-08-09 22:34:22 +0900

    fix: add job categories and speed up

--- jobplanet_scraper/jobplanet_api.py
+++ jobplanet_scraper/jobplanet_api.py
@@ -12,107 +12,85 @@ load_dotenv()
 
 
 class Scraper:
-    def __init__(self, base_url)-> None:
+    def __init__(self, base_url: str, category_id: int, category_name: str) -> None:
         self.base_url = base_url
-        self.category_dict = {
-            11605: 'DBA',
-            11907: 'iOS',
-            11601: 'QA',
-            11908: '기술지원',
-            11609: '네트워크/보안/운영',
-            11904: '백엔드 개발',
-            11607: '소프트웨어 개발',
-            11906: '안드로이드 개발',
-            11611: '웹퍼블리셔',
-            11905: '프론트엔드 개발',
-            11608: '하드웨어 개발',
-            11613: '데이터 분석가',
-            11914: '데이터 사이언티스트',
-            11913: '데이터 엔지니어',
-            11915: '머신러닝 엔지니어'
-        }
+        self.category_id = category_id
+        self.category_name = category_name
+        self.job_descriptions = []
+        
 
-
-    async def fetch_data_from_api(self, api_url: str) -> Dict[str, Any]:
+    async def fetch_data_from_api(self, session, api_url: str) -> Dict[str, Any]:
         """
         API로부터 response를 확인하고 data를 가져옵니다.
         """
-        async with aiohttp.ClientSession() as session:
-            async with session.get(api_url) as response:
-                if response.status != 200:
-                    print(f"Error occurred: {response.status}")
-                    return {}
-                response_text = await response.text(encoding="utf-8")
-                data = json.loads(response_text)
+        async with session.get(api_url) as response:
+            if response.status != 200:
+                print(f"Error occurred: {response.status}")
+                return {}
+            response_text = await response.text(encoding="utf-8")
+            data = json.loads(response_text)
 
-                return data
+            return data
 
 
-    async def get_job_lists(self, base_url: str, job_category_id: int) -> Dict[int, Any]:
+    async def get_job_lists(self, session, base_url: str) -> List[int]:
         """
-        해당 카테고리(job_type_id)에 맞는 공고 job_id list를 얻어냅니다.
+        해당 카테고리에 맞는 공고 job_id list를 얻어냅니다.
         """
         page_size = 9
         result = await self.fetch_data_from_api(
-            f'{base_url}api/v3/job/postings?order_by=recent&occupation_level2={job_category_id}&page=1&page_size={page_size}'
+            session,
+            f'{base_url}api/v3/job/postings?order_by=recent&occupation_level2={self.category_id}&page=1&page_size={page_size}'
         )
         total_page_cnt_float = result['data']['total_count'] / page_size
         total_page_cnt = math.ceil(total_page_cnt_float)
 
-        job_lists_val = []
+        job_id_lists = []
         for page in range(1, total_page_cnt + 1):
             result = await self.fetch_data_from_api(
-                f'{base_url}api/v3/job/postings?order_by=recent&occupation_level2={job_category_id}&page={page}&page_size={page_size}'
+                session,
+                f'{base_url}api/v3/job/postings?order_by=recent&occupation_level2={self.category_id}&page={page}&page_size={page_size}'
             )
-            job_lists_val.extend([recruit['id'] for recruit in result['data']['recruits']])
+            job_id_lists.extend([recruit['id'] for recruit in result['data']['recruits']])
             await asyncio.sleep(1)
-        
-        job_lists = {}
-        job_lists[job_category_id] = job_lists_val
 
-        return job_lists
+        return job_id_lists
+    
 
-
-    async def get_job_descriptions(self, base_url :str, job_category_id: int, jd_lists_val: Dict[int, Any]) -> List[Dict[str, Any]]:
+    async def get_job_descriptions(self, session, base_url :str, job_id: int) -> None:
         """
         카테고리별로 수집한 JD list를 통해 각 JD의 정보를 얻어냅니다.
         """
-        job_descriptions = []
-        job_lists = jd_lists_val[job_category_id]
+        result = await self.fetch_data_from_api(
+            session,
+            f'{base_url}api/v1/job/postings/{job_id}'
+        )
 
-        for i in range(0, len(job_lists)):
-            post_id = job_lists[i]
-            result = await self.fetch_data_from_api(
-                f'{base_url}api/v1/job/postings/{post_id}'
-            )
-
-            end_at_text = result['data']['end_at']
-            date_object = datetime.strptime(end_at_text, '%Y.%m.%d')
-            end_at_text = date_object.strftime('%Y-%m-%d')
-            
-            val = {
-                'job_id': str(post_id),
-                'platform': 'jobplanet',
-                'category': self.category_dict[job_category_id],
-                'company': result['data']['name'],
-                'title': result['data']['title'],
-                'preferred': None if result['data']['preferred_skill'] == "" else result['data']['preferred_skill'].replace("\r\n", "\n"),
-                'required': result['data']['required_qualification'].replace("\r\n", "\n"),
-                'primary_responsibility': result['data']['primary_responsibility'].replace("\r\n", "\n"),
-                'url': f'https://jobplanet.co.kr/job/search?posting_ids%5B%5D={post_id}',
-                'end_at': end_at_text,
-                'skills': result['data']['skills'],
-                'location': None if result['data']['location'] == "" else result['data']['location'],
-                'welfare': None if result['data']['benefit'] == "" else result['data']['benefit'].replace("\r\n", "\n"),
-                'body': None,
-                'company_description': None if result['data']['introduction'] == "" else result['data']['introduction'].replace("\r\n", "\n"),
-                'coordinate': None,
-            }
-
-            job_descriptions.append(val)
-            await asyncio.sleep(2)
+        end_at_text = result['data']['end_at']
+        date_object = datetime.strptime(end_at_text, '%Y.%m.%d')
+        end_at_text = date_object.strftime('%Y-%m-%d')
         
-        return job_descriptions
+        val = {
+            'job_id': str(job_id),
+            'platform': 'jobplanet',
+            'category': self.category_name,
+            'company': result['data']['name'],
+            'title': result['data']['title'],
+            'preferred': None if result['data']['preferred_skill'] == "" else result['data']['preferred_skill'].replace("\r\n", "\n"),
+            'required': result['data']['required_qualification'].replace("\r\n", "\n"),
+            'primary_responsibility': result['data']['primary_responsibility'].replace("\r\n", "\n"),
+            'url': f'https://jobplanet.co.kr/job/search?posting_ids%5B%5D={job_id}',
+            'end_at': end_at_text,
+            'skills': result['data']['skills'],
+            'location': None if result['data']['location'] == "" else result['data']['location'],
+            'welfare': None if result['data']['benefit'] == "" else result['data']['benefit'].replace("\r\n", "\n"),
+            'body': None,
+            'company_description': None if result['data']['introduction'] == "" else result['data']['introduction'].replace("\r\n", "\n"),
+            'coordinate': None,
+        }
+
+        self.job_descriptions.append(val)
+        await asyncio.sleep(2)
 
 
     @staticmethod
@@ -139,7 +117,9 @@ class Scraper:
 
     @staticmethod
     def upload_to_s3(file_path: str, bucket_name: str, access_key: str, secret_key: str, region_name: str) -> None:
-        '''Uploads the specified file to an AWS S3 bucket.'''
+        """
+        Uploads the specified file to an AWS S3 bucket.
+        """
 
         print("Start upload!")
         
@@ -161,17 +141,13 @@ class Scraper:
         """
         Scraper Class의 main 함수입니다. 잡플래닛 JD 수집을 비동기로 실행합니다.
         """
-        total_jd_list = []
-
-        tasks = [self.get_job_lists(self.base_url, job_type_id) for job_type_id in self.category_dict.keys()]
-        values = await asyncio.gather(*tasks)
+        async with aiohttp.ClientSession() as session:
+            job_id_lists = await self.get_job_lists(session, self.base_url)
+            
+            tasks = [self.get_job_descriptions(session, self.base_url, job_id) for job_id in job_id_lists]
+            await asyncio.gather(*tasks)
         
-        tasks = [self.get_job_descriptions(self.base_url, job_type_id, value) for job_type_id, value in zip(self.category_dict.keys(), values)]
-        results = await asyncio.gather(*tasks)
-        for result in results:
-            total_jd_list.extend(result)
-
-        return total_jd_list
+        return self.job_descriptions
 
 
 def main() -> None:
@@ -180,13 +156,47 @@ def main() -> None:
     secret_key = os.environ.get('secret_key')
     region_name = "ap-northeast-2"
 
+    job_category_dict = {
+            11909: 'CTO',
+            11605: 'DBA',
+            11615: 'ERP',
+            11907: 'iOS',
+            11601: 'QA',
+            11910: 'VR 엔지니어',
+            11610: '게임개발',
+            11908: '기술지원',
+            11609: '네트워크/보안/운영',
+            11904: '백엔드 개발',
+            11607: '소프트웨어 개발',
+            11906: '안드로이드 개발',
+            11611: '웹퍼블리셔',
+            11905: '프론트엔드 개발',
+            11608: '하드웨어 개발',
+            11917: 'BI 엔지니어',
+            11613: '데이터 분석가',
+            11914: '데이터 사이언티스트',
+            11913: '데이터 엔지니어',
+            11915: '머신러닝 엔지니어',
+            11916: '빅데이터 엔지니어'
+        }
+
     base_url = 'https://www.jobplanet.co.kr/'
-    scraper = Scraper(base_url=base_url)
+
+    tasks = []
+    for category_id, category_name in job_category_dict.items():
+        scraper = Scraper(base_url=base_url, category_id=category_id, category_name=category_name)
+        task = scraper.main()
+        tasks.append(task)
+
     loop = asyncio.get_event_loop()
-    total_jd_list = loop.run_until_complete(scraper.main())
+    data_list = loop.run_until_complete(asyncio.gather(*tasks))
+    
+    result = []
+    for data in data_list:
+        result.extend(data)
 
-    file_path = scraper.save_json(total_jd_list)
-
+    file_path = Scraper.save_json(result)
+    
     Scraper.upload_to_s3(
         file_path = file_path,
         bucket_name=bucket_name,
