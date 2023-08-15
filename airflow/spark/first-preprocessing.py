10392e74ce8f3abecceea68ba7ae9df3e613469e
heewoneha <jhjmo0719h@naver.com>
2023-08-16 00:28:20 +0900

    feat: coordinate, category spark 1st preprocessing

--- airflow/spark/first-preprocessing.py
+++ airflow/spark/first-preprocessing.py
@@ -1,22 +1,33 @@
 from typing import List
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import regexp_replace
-from pyspark.sql.types import StringType, StructField, StructType
+from pyspark.sql.types import StringType, StructField, StructType, ArrayType, FloatType
+from pyspark.sql import functions as F
+from pyspark.sql.functions import monotonically_increasing_id
 import os
 import json
 import boto3
 import glob
+import requests
 from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv
+from datetime import date
+
 
 load_dotenv()
+
 
 class S3Downloader:
     def __init__(self, bucket_name, access_key, secret_key):
         self.bucket_name = bucket_name
         self.access_key = access_key
         self.secret_key = secret_key
-        self.s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+        self.s3 = boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name="ap-northeast-2"
+        )
 
     def download_file(self, remote_path, local_path):
         try:
@@ -24,9 +35,29 @@ class S3Downloader:
         except NoCredentialsError:
             raise Exception("AWS credentials not available")
 
+
 class Preprocessing:
-    def __init__(self):
+    def __init__(self, kakao_api_token):
         self.spark = SparkSession.builder.appName("Python Spark preprocessing #1").master("local").getOrCreate()
+        self.headers = {'Authorization': 'KakaoAK ' + kakao_api_token}
+    
+    def get_coordinate_from_location(self, location: str) -> List[float]:
+        """
+        카카오 API를 통해 location으로부터 coordinate(lat, lon) 리스트를 반환합니다.
+        """
+        if location is None:
+            return None
+        
+        url = f'https://dapi.kakao.com/v2/local/search/address.json?query={location}'
+        
+        try:
+            response = requests.get(url, headers=self.headers, timeout=5)
+            result = json.loads(response.text)
+            match_first = result['documents'][0]['address']
+            return [float(match_first['y']), float(match_first['x'])]
+
+        except (requests.exceptions.RequestException, TypeError, ValueError, KeyError, IndexError) as e:
+            print(f'Error occurred: {e} while fetching address: {location}')
 
     def read_json_files(self, file_paths: List[str]) -> DataFrame:
         data = []
@@ -34,31 +65,31 @@ class Preprocessing:
             with open(path) as json_file:
                 json_data = json.load(json_file)
                 # 키 이름 변경
-                if "result" in json_data:
+                if "results" in json_data:
                     data.append(path)
         
         # 모든 JSON 파일을 한 번에 읽어 DataFrame 생성
         df = self.spark.read.option("multiline", "true").json(data)
         
-        df_final = df.selectExpr("explode(result) as result")
+        df_final = df.selectExpr("explode(results) as results")
 
         df_final = df_final.select(
-            "result.job_id",
-            "result.platform",
-            "result.category",
-            "result.url",
-            "result.company",
-            "result.title",
-            "result.primary_responsibility",
-            "result.required",
-            "result.preferred",
-            "result.end_at",
-            "result.skills",
-            "result.location",
-            "result.welfare",
-            "result.body",
-            "result.company_description",
-            "result.coordinate"
+            "results.job_id",
+            "results.platform",
+            "results.category",
+            "results.url",
+            "results.company",
+            "results.title",
+            "results.primary_responsibility",
+            "results.required",
+            "results.preferred",
+            "results.end_at",
+            "results.skills",
+            "results.location",
+            "results.welfare",
+            "results.body",
+            "results.company_description",
+            "results.coordinate"
         )
         
         return df_final
@@ -71,21 +102,67 @@ class Preprocessing:
     def stop_spark_session(self):
         self.spark.stop()
 
+
+class S3Uploader:
+    def __init__(self, bucket_name, access_key, secret_key):
+        self.bucket_name = bucket_name
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.s3 = boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name="ap-northeast-2"
+        )
+    
+    def upload_file(self, platform_name):
+        """
+        1차 전처리된 최종 parquet 파일을 s3에 업로드합니다.
+        """
+        s3_key = get_file_path(platform_name, 'parquet')
+
+        folder_path = f'data/{platform_name}.parquet'
+
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                local_path = os.path.join(root, file)
+                s3_path = os.path.relpath(local_path, folder_path)
+                result_s3_key = f"{s3_key}/{s3_path}"
+                
+                try:
+                    self.s3.upload_file(local_path, self.bucket_name, result_s3_key)
+                except NoCredentialsError:
+                    raise Exception("AWS credentials not available")
         
+
+def get_file_path(platform_name: str, file_type: str) -> str:
+    """
+    s3_key가 될 file_path를 생성합니다.
+    """
+    today = date.today()
+    year = str(today.year)
+    month = str(today.month).zfill(2)
+    day = str(today.day).zfill(2)
+
+    return f'{platform_name}/year={year}/month={month}/day={day}/{platform_name}.{file_type}'
+
+
 def main():
     BUCKET_NAME = os.getenv('BUCKET_NAME')
     ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
     SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    KAKAO_API_TOKEN = os.getenv('KAKAO_API_TOKEN')
 
     downloader = S3Downloader(BUCKET_NAME, ACCESS_KEY, SECRET_KEY)
 
     os.makedirs('data', exist_ok=True)
-    downloader.download_file('jobplanet/year=2023/month=08/day=14/jobplanet.json', 'data/jobplanet.json')
-    downloader.download_file('jumpit/year=2023/month=08/day=12/jumpit.json', 'data/jumpit.json')
-    downloader.download_file('rallit/year=2023/month=08/day=12/rallit.json', 'data/rallit.json')
-    downloader.download_file('wanted/year=2023/month=08/day=12/wanted.json', 'data/wanted.json')
 
-    spark_preprocessor = Preprocessing()
+    platform_list = ['jobplanet', 'jumpit', 'rallit', 'wanted']
+    for platform in platform_list:
+        json_file_path = get_file_path(platform,'json')
+        downloader.download_file(json_file_path, f'data/{platform}.json')
+
+    spark_preprocessor = Preprocessing(KAKAO_API_TOKEN)
 
     file_paths = glob.glob(f'{os.getcwd()}/data/*.json')
 
@@ -93,11 +170,7 @@ def main():
         raise ValueError("No JSON files found in the specified directory!")
 
     df_final = spark_preprocessor.read_json_files(file_paths)
-
-    # 결과 확인
-    df_final.show()
-
-    data_list = []
+    
     # 대분류, 중분류, 카테고리를 나타내는 데이터
     categories = [
     ("WEB", "서버/백엔드 개발자", ["서버 개발자", "자바 개발자", "Node.js 개발자", "PHP 개발자", "웹 개발자", "루비온레일즈 개발자", ".NET 개발자"]),
@@ -124,6 +197,7 @@ def main():
     ("ETC", "기타", ["ERP전문가", "CTO,Chief Technology Officer", "ERP"])
     ]
 
+    data_list = []
     for major_category, middle_category, job_list in categories:
         for sub_category in job_list:
             data_list.append((major_category, middle_category, sub_category))
@@ -137,7 +211,11 @@ def main():
 
     df_with_mapped_categories = df_final.join(mapping_df, df_final.category == mapping_df.sub_category, "left")
 
-    new_columns = ['job_id', 'platform', 'category', 'major_category', 'middle_category', 'sub_category', 'company', 'title', 'preferred', 'required', 'primary_responsibility', 'url', 'end_at', 'skills', 'location', 'welfare', 'body', 'company_description', 'coordinate']
+    new_columns = [
+        'job_id','platform', 'category', 'major_category', 'middle_category', \
+        'sub_category', 'company', 'title', 'preferred', 'required', 'primary_responsibility', \
+        'url', 'end_at', 'skills', 'location', 'welfare', 'body', 'company_description', 'coordinate'
+    ]
 
     def rearrange_dataframe_columns(df: DataFrame, columns: List[str]) -> DataFrame:
         return df.select(*columns)
@@ -147,12 +225,31 @@ def main():
     text_columns = ['preferred', 'required', 'primary_responsibility', 'welfare', 'company_description']
     df_final = spark_preprocessor.process_text_columns(df_with_mapped_categories, text_columns)
 
-    output_path = "data/cleaned_data.parquet"
-    df_final.write.parquet(output_path, mode="overwrite")
+    df_final_with_id = df_final.withColumn('id', monotonically_increasing_id())
+
+    df_filter_for_coordinate = df_final_with_id.filter((df_final_with_id['platform'] == 'jobplanet') | (df_final_with_id['platform'] == 'jumpit'))
+    
+    get_coordinate_udf = F.udf(spark_preprocessor.get_coordinate_from_location, ArrayType(FloatType()))
+    df_with_coordinate = df_filter_for_coordinate.withColumn('coordinate', get_coordinate_udf(df_filter_for_coordinate['location']))
+
+    result_with_id_df = df_final_with_id.join(
+        df_with_coordinate, on='id', how='left'
+    )
+
+    result_df = result_with_id_df.drop('id')
+
+    output_path = "data/1st_cleaned_data.parquet"
+    result_df.write.parquet(output_path, mode="overwrite")
 
     spark_preprocessor.stop_spark_session()
     
-    print('finish')
+    print('Spark Finish')
+
+    uploader = S3Uploader(BUCKET_NAME, ACCESS_KEY, SECRET_KEY)
+    uploader.upload_file('1st_cleaned_data')
+
+    print('Upload Finish')
+    
 
 if __name__ == "__main__":
     main()
