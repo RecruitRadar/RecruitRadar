93e95227d8eebdde66dbbec86e9d65835c56992d
daewonseo <saz0549@naver.com>
2023-08-21 10:46:56 +0900

    feat: add airflow dag for glue nlp crawler and job

---
+++ airflow/dags/glue_nlp_crawler.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+import time
+import boto3
+from airflow import DAG
+from airflow.models import Variable
+from airflow.decorators import task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+
+DEFAULT_ARGS = {
+    'owner': 'jd_analysis',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'catchup': False, # True로 설정하면, start_date 이전의 DAG도 실행함
+    'retries': 1,
+}
+
+def get_airflow_variable_or_default(variable_name, default_value=None):
+    try:
+        return Variable.get(variable_name)
+    except KeyError:
+        return default_value
+
+with DAG('glue_nlp_crawler_dag', 
+         description='Run Glue NLP Crawler',
+         schedule_interval=None,
+         start_date=datetime(2023, 8, 15),
+         default_args=DEFAULT_ARGS) as dag:
+    
+    @task()
+    def start_crawler(crawler_name:str) -> str:
+        access_key = get_airflow_variable_or_default("access_key")
+        secret_key = get_airflow_variable_or_default("secret_key")
+        region_name = get_airflow_variable_or_default("region_name")
+        glue_client = boto3.client('glue', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region_name)
+        glue_client.start_crawler(Name=crawler_name)
+        
+        message = f"Start crawler: {crawler_name}"
+        print(f"Start crawler: {crawler_name}")
+        
+        return message
+
+        
+    @task()
+    def check_crawler_status(crawler_name:str) -> str:
+        '''
+        avoid READY, RUNNING, STOPPING status
+        '''
+        
+        access_key = get_airflow_variable_or_default("access_key")
+        secret_key = get_airflow_variable_or_default("secret_key")
+        region_name = get_airflow_variable_or_default("region_name")
+        glue_client = boto3.client('glue', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region_name)
+        response = glue_client.get_crawler(Name=crawler_name)['Crawler']['State']
+        
+        if response == 'READY':
+            raise Exception("Crawler is in READY state. Please check the crawler.")
+        else:
+            while response in ['RUNNING', 'STOPPING']:
+                response = glue_client.get_crawler(
+                    Name=crawler_name
+                    )['Crawler']['State']
+                print(f"Current status: {response}")
+                time.sleep(4)
+            
+            else:
+                print(f"Current status: {response}")
+                return f"Current status: {response}" 
+    
+    @task()
+    def log_final_message() -> None:
+        print("All crawlers have completed successfully!")
+    
+    
+    
+    trigger_glue_nlp_dag_task = TriggerDagRunOperator(
+        task_id='trigger_glue_nlp_dag',
+        trigger_dag_id="glue_nlp_job_dag",  # 여기에 트리거링하려는 DAG의 ID를 지정합니다.
+        conf={"message": "Triggered from glue_nlp_crawler_dag"},
+        execution_date="{{ ds }}", # 여기에 트리거링하려는 DAG의 execution_date를 지정합니다. {ds}
+        reset_dag_run=True,
+        wait_for_completion=True,
+        poke_interval=60,
+    )
+    
+    crawler_name = 'de1-1-1st-cleaned-data-crawler'
+    
+    start_crawler(crawler_name=crawler_name) >> check_crawler_status(crawler_name=crawler_name) >> log_final_message() >> trigger_glue_nlp_dag_task
--- airflow/dags/glue_nlp_job.py
+++ airflow/dags/glue_nlp_job.py
@@ -4,7 +4,6 @@ import boto3
 from airflow import DAG
 from airflow.models import Variable
 from airflow.decorators import task
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
 DEFAULT_ARGS = {
@@ -25,55 +24,46 @@ def create_glue_client():
     region_name = get_airflow_variable("region_name")
     return boto3.client('glue', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region_name)
 
-with DAG('glue_etl_job_dag',
-         description='Run Glue ETL Job',
+with DAG('glue_nlp_job_dag',
+         description='Run Glue NLP Job',
          schedule_interval=None,
          start_date=datetime(2023, 8, 15),
          default_args=DEFAULT_ARGS) as dag:
 
     @task()
-    def start_etl_job(job_name: str) -> str:
+    def start_nlp_job(job_name: str) -> str:
         glue_client = create_glue_client()
+        print(glue_client)
         response = glue_client.start_job_run(JobName=job_name)
         return response['JobRunId']
 
     @task()
-    def monitor_etl_job(job_name: str, job_run_id: str) -> str:
+    def monitor_nlp_job(job_name: str, job_run_id: str) -> str:
         glue_client = create_glue_client()
         response = glue_client.get_job_run(JobName=job_name, RunId=job_run_id)
         job_run_state = response['JobRun']['JobRunState']
 
         while job_run_state in ['RUNNING', 'STOPPING']:
-            print(f"ETL Job status: {job_run_state}")
-            time.sleep(4)
+            print(f"NLP Job status: {job_run_state}")
+            time.sleep(10)
             response = glue_client.get_job_run(JobName=job_name, RunId=job_run_id)
             job_run_state = response['JobRun']['JobRunState']
 
             if job_run_state == 'FAILED':
-                raise Exception(f"ETL Job failed with status: {job_run_state}")
+                raise Exception(f"NLP Job failed with status: {job_run_state}")
 
         return job_run_state
 
 
     @task()
     def log_final_message(job_run_state:str) -> None:
-        print(f"ETL Job finished with status with {job_run_state}!")
+        print(f"NLP Job finished with status with {job_run_state}!")
         
-        return f"ETL Job finished with status with {job_run_state}!"
-
-    trigger_glue_nlp_crawler_dag_task = TriggerDagRunOperator(
-        task_id='trigger_glue_nlp_crawler_dag',
-        trigger_dag_id="glue_nlp_crawler_dag",  # 여기에 트리거링하려는 DAG의 ID를 지정합니다.
-        conf={"message": "Triggered from glue_etl_job_dag"},
-        execution_date="{{ ds }}", # 여기에 트리거링하려는 DAG의 execution_date를 지정합니다. {ds}
-        reset_dag_run=True,
-        wait_for_completion=True,
-        poke_interval=60,
-    )
+        return f"NLP Job finished with status with {job_run_state}!"
         
-    etl_job_name = 'de1_1_1st_preprocessing_script'
-    job_run_id_extract = start_etl_job(job_name=etl_job_name)
-    monitor_task = monitor_etl_job(job_name=etl_job_name, job_run_id=job_run_id_extract)
+    nlp_job_name = 'de1_1_2nd_preprocessing_script'
+    job_run_id_extract = start_nlp_job(job_name=nlp_job_name)
+    monitor_task = monitor_nlp_job(job_name=nlp_job_name, job_run_id=job_run_id_extract)
     log_final_message_task = log_final_message(monitor_task)
 
-    job_run_id_extract >> monitor_task >> log_final_message_task >> trigger_glue_nlp_crawler_dag_task
+    job_run_id_extract >> monitor_task >> log_final_message_task
